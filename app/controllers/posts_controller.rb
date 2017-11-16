93d8f1be29e40cf0ec6541e65d7e079e5ac3090f
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-16 16:02:50 -0500

    Add gem devise and links to log in, sign up and sign out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+#  before_action :authenticate_user! #No deja entrar a nada si no estoy logeado
+
   def index
+    @posts = Post.all
   end
+  def show
+    @post = Post.find(params[:id])
+  end
+
 end
