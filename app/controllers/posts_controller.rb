10c76106e4db320d2ac466a8f23ff1083f9c2ab6
Edgardo David <edgardo.burgos94@gmail.com>
2017-11-17 11:28:25 -0500

    Fix links 'editar' and 'eliminar' when user is not log in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 #  before_action :authenticate_user! #No deja entrar a nada si no estoy logeado
 
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC")
   end
 
   def show
